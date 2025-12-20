ba2ca20bad9c1886133179c89a047b82001883ff
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2025-12-20 19:56:18 +0500

    hammad working with loops today

---
+++ Day3.js
@@ -0,0 +1,17 @@
+// Basic Loops :
+//print numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// print only even numbers from 1 to 20
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log("Even Numbers:", i);
+  }
+}
+
+// print numbers in reverse 10 to 1;
+for (let i = 10; i >= 1; i--) {
+  console.log("Reverse Numbers:", i);
+}
