636f247f1d81a90fa0bd04035badd0035b036c2d
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2026-01-17 10:32:15 +0500

    Hammad working with js to strong basics

---
+++ JanuaryJavaScript/JanDay1.js
@@ -0,0 +1,19 @@
+// *************************** Start Day 1 of January JavaScript Challenge ***************************
+// =>1.Variables
+//    Variables are the cotainers for storing data values.
+//    =>1. let is the blocked Scope and can be reassigned not redeclared.
+let age = 23;
+age = 26; //so this is the reassignment and this is allowed using let keyword.
+
+//    =>2.const is the bocked scopr butt cant be reassigned or redeclared.
+const pi = 3.14159;
+pi = 3.14; // this will gives an error because of the reassignment using const keyword.
+//    =>3.var is the function that is reassigned and redeclared.
+var city = "New York";
+var city = "Los Angeles"; // this is redeclaration and this is allowed using var keyword.
+city = "Chicago"; // this is reassignment and this is allowed using var keyword.
+
+//Variables Must start with letter, underscore and the $ sign.
+// Can contain letters, numbers, underscores, dollar signs
+// Cannot use reserved keywords (like let, if, function)
+// Case-sensitive (myVar ≠ myvar)
