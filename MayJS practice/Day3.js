a9faaf7413fc43fef158a9494b04706d195ef834
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2026-06-02 02:38:27 +0500

    Hammad is here

---
+++ MayJS practice/Day3.js
@@ -0,0 +1,11 @@
+// Loops and iterations
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// Looping An Array
+let fruits = ["Apple", "Banana", "Mangoe", "Water-melon"];
+
+for (let j = 0; j < fruits.length; j++) {
+  console.log(fruits[j]);
+}
