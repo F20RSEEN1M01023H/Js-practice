74bb2962a71e3410c45b033e561534d004091f06
F20RSEEN1M01023H <hammadchoudharyy@gmail.com>
2026-06-19 03:41:57 +0500

    Hmmad is her

--- MayJS practice/Day7.js
+++ MayJS practice/Day7.js
@@ -34,3 +34,35 @@
 // car.accelration();
 // car.accelration();
 // car.accelration();
+
+// Concept 3 *****************************
+// const user = {
+//   name: "Hammad Ashraf",
+//   age: 23,
+//   greet() {
+//     console.log(this.name);
+//   },
+// };
+// user.greet();
+// const fn = user.greet();
+// fn(); // error that fn is not a function
+
+// Another example *******************
+// const timer = {
+//   message: "This message is timer",
+//   start() {
+//     setTimeout(function() {
+//       console.log(this.message);
+//     }, 1000);
+//   },
+// };
+// timer.start();
+
+//Concept 4**************************** This in regular functions
+function showThis() {
+  console.log(this);
+}
+
+showThis();
+const object = { showThis };
+object.showThis();
